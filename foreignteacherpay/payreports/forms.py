559d028b5dd7ac0905fd7f92588e965ac9f58ad8
Luke Petschauer <luke@lukewrites.com>
2014-11-29 22:35:20 +0800

    start on forms

--- foreignteacherpay/payreports/forms.py
+++ foreignteacherpay/payreports/forms.py
@@ -3,12 +3,18 @@ from foreignteacherpay.payreports.models import Teacher, Employer
 
 
 class teacherForm(ModelForm):
+
+
+
     class Meta:
         model = Teacher
         fields = [user_name, first_name, last_name, password, password_check, email]
 
 
 class employerForm(ModelForm):
+
+
+
     class Meta:
         model = Employer
         fields = [school_name, school_city, school_province, school_experience, recommend_school]
